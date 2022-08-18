e3c6f3dbf81c4ea5a75ae8ee1df58f69d3ed0de0
Luis-cast <78997814+Luis-cast@users.noreply.github.com>
2022-08-17 22:34:05 -0500

    Add files via upload

---
+++ tarea3LuisCastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+   echo "<h1>PROGRAMACIÓN IV</h1>";
+    echo "<h2>VARIBLES Y TIPOS DE DATOS</h2>";
+    echo "<div style= background-color:yellow>
+        UNIVERSIDAD LATINA DE PANAMÁ 
+    </div>";
+    
+?> 
+
+</body>
+<footer>
+    <?php
+        echo "<div style= background-color:green>
+            Este es el pie de pagina
+        </div>";
+    ?> 
+</footer>
+</html>
